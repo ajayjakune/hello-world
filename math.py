b737a6f1fe8198f967cd45a64f094e5d3b2ce01e
ajayjakune <ajayjakune@gmail.com>
2021-01-25 14:59:52 +0530

    Added math.py

---
+++ math.py
@@ -0,0 +1,19 @@
+# Perform addition
+def add(x,y):
+    pass
+
+# Perform subtraction
+def subtract(x,y):
+    pass
+
+# Perform multiplication
+def multiply(x,y):
+    pass
+    
+
+
+# Perform divisions
+def divide(x,y):
+    pass
+
+
